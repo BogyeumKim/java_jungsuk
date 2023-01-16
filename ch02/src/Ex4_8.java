93a8970b61057f4e396980c91d95612c4a0105a5
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-16 20:26:20 +0900

    반복문

---
+++ ch02/src/Ex4_8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Ex4_8 {
+
+	public static void main(String[] args) {
+
+//		for(int i=0; i<3; i++) {
+		for(int i=0,j=10; i<3; i++,j--) { // 타입같아야 i,j 증감 및 감소 가능
+			System.out.println("i="+i+",j="+j);
+		}
+
+	}
+
+}

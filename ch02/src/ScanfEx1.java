b8f472ff576e118d42a2f05893a3cfad4b42c2af
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-16 15:28:21 +0900

    Scanner
    오랜만에 본다.. 사실상 input이나 js로 form에 user가 입력한 값을 보내지만..

---
+++ ch02/src/ScanfEx1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class ScanfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+//		System.out.println("입력해 :");
+//		int num = scan.nextInt();
+//		System.out.println(num);
+		
+		String input = scan.nextLine(); 
+		int um = Integer.parseInt(input);
+		
+		System.out.println(um);
+		
+		
+		
+	}
+
+}

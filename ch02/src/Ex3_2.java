916a4cd30d2c793d06d4db74520b44f3d2b6c4e6
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-16 16:50:36 +0900

    증감연산자

---
+++ ch02/src/Ex3_2.java
@@ -0,0 +1,21 @@
+
+public class Ex3_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5,j=0;
+		
+		j=i++;
+		System.out.println("i="+i+"j="+j);
+
+		i=5;
+		j=0;
+		
+		j = ++i;
+		System.out.println("i="+i+"j="+j);
+		
+		
+
+	}
+
+}

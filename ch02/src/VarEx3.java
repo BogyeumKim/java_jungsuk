98dbfc7f9a4268697189734b5fccc5c18fe38fa7
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-16 14:01:30 +0900

    숫자type
    jdk1.8부터 long 타입에 언더바 가능

--- ch02/src/VarEx3.java
+++ ch02/src/VarEx3.java
@@ -5,9 +5,29 @@ public class VarEx3 {
 		// TODO Auto-generated method stub
 		final int score = 100;
 //		score = 200;
+		boolean power = true;
+		
+		byte b = 5; // - 128 ~ 127 
+		
+		int oct = 010; // 8진수
+		int hex = 0x10; // 16진수
+		
+		long l = 10_000_000_000L; // jdk1.8부터 언더바(_) 가능
+		
+		float f = 3.14f;
+		double d = 3.14;
+		
+		System.out.println(f);
+		System.out.println(d);
+		
+		System.out.println(l);
+		
+		System.out.println(oct);
+		System.out.println(hex);
 		
 		
-		System.out.println(score);asdads
-
+		System.out.println(b);
+		System.out.println(power);
+		System.out.println(score);
 	}
 }

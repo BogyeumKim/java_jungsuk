f56e8dd05d658d1e3dc9ebcea9687a5f3de9bc45
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-17 14:19:20 +0900

    main함수에 매개변수 넣기

---
+++ ch02/src/Ex5_7.java
@@ -0,0 +1,15 @@
+ import java.util.Arrays; // 컨 쉬프트 o
+
+public class Ex5_7 {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("매개변수 개수:" + args.length);
+		
+		for(int i=0; i<args.length; i++) {
+			System.out.println(args[i]+"\n");
+		}
+		
+	}
+
+}

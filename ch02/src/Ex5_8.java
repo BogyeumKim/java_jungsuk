f0619153622bd284e2e4bf0f5fe8c97b25dd5f49
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-17 18:13:57 +0900

    이중배열 가로세로합

--- ch02/src/Ex5_8.java
+++ ch02/src/Ex5_8.java
@@ -3,35 +3,35 @@ import java.util.Arrays;; // 컨 쉬프트 o
 public class Ex5_8 {
 
 	public static void main(String[] args) {
- 
-		int[][] score = {
-				{100, 100 ,100},
-				{20,20,20},
-				{30,30,30},
-				{40,40,40}
-		};
-		
+
+		int[][] score = { { 100, 100, 100 },
+				{ 20, 20, 20 },
+				{ 30, 30, 30 },
+				{ 40, 40, 40 } };
+
+		int[] xarry = new int[score.length];
+		int[] yarry = new int[score[0].length];
 		int sum = 0;
-		int asd = 0;
-		
-		for (int i=0; i<score.length; i++) {
-			for(int j=0; j<score[i].length; j++) {
+
+		for (int i = 0; i < score.length; i++) { // score.length = 4 
+			for (int j = 0; j < score[i].length; j++) { // score[i].length =3 , j = 0,1,2
 //				System.out.printf("score[%d][%d] = %d\n",i,j,score[i][j]);
-				sum +=score[i][j];
+				
+				sum += score[i][j];
+				xarry[i] += score[i][j];
+				yarry[j] += score[i][j];
+				
 			}
-			System.out.println(sum);
-			sum=0; // 가로한번 끝나고 초기화해야하니까.
-			System.out.println("================");
 		}
-		
-		
-		
+		System.out.println("합계 :" + sum);
+		System.out.println("================");
 
+		System.out.println("가로의합 : " + Arrays.toString(xarry));
 
-		
-//		System.out.println("sum ="+sum);
-		
+		System.out.println("================");
+
+		System.out.println("세로의합 : " + Arrays.toString(yarry));
+
 	}
 
 }
- 

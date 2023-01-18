0fe055fa39e2f80eacd6d0032836312153248761
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-01-18 22:32:45 +0900

    계산기 클래스

---
+++ ch06/src/Ex6_4.java
@@ -0,0 +1,53 @@
+
+public class Ex6_4 {
+	
+	public static void main(String args[]) {
+				MyMath mm = new MyMath();
+				long reuslt1 = mm.add(5L, 3L);
+				long result2 = mm.subtract(10L, 20);
+				long result3 = mm.multiply(8, 4L);
+				long result4 = mm.divide(10, 5);
+				int result5 = mm.maxVal(5, 4);
+				
+				System.out.println(reuslt1);
+				System.out.println(result2);
+				System.out.println(result3);
+				System.out.println(result4);
+				System.out.println(result5);
+	
+		
+	}
+		
+	
+}
+
+class MyMath {
+	
+	
+	long add (long a, long b) {
+		long result = a+b;
+		return result; // or return a+b
+	}
+	
+	long subtract(long a,long b) {
+		return a-b;
+	}
+	
+	long multiply(long a,long b) {
+		return a*b;
+	}
+	
+	long divide(long a,long b) {
+		return a/b;
+	}
+	
+	// 두 값 받아서 둘중에 큰값 반환하는 메소드 작성하기
+	int maxVal(int a,int b) {
+		if (a<b) return b;
+		else return a;		
+	}
+}
+
+
+
+

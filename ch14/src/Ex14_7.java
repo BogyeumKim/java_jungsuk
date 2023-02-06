16c29fcc8e6e5c9bbd5e42522aeb79ba21def717
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-02-07 01:16:59 +0900

    Stream flatMap
    주석 추가

--- ch14/src/Ex14_7.java
+++ ch14/src/Ex14_7.java
@@ -7,11 +7,11 @@ public class Ex14_7 {
 	public static void main(String[] args) {
 		Stream<String[]> strArrStrm = Stream.of(new String[] {"abc","def","jkl"},new String[] {"ABC","DEF","JKL"});
 		
-		Stream<String> strStrm = strArrStrm.flatMap(Arrays::stream);
+		Stream<String> strStrm = strArrStrm.flatMap(Arrays::stream); // abc ,def 가 하나의 flatMap으로 하나의 Stream으로 모아짐
 		
-		strStrm.map(String::toLowerCase)
-			.distinct()
-			.sorted()
+		strStrm.map(String::toLowerCase) // 소문자로 변경 
+			.distinct() // 중복제거
+			.sorted() // 정렬
 			.forEach(System.out::println);
 		
 		System.out.println();
@@ -21,11 +21,11 @@ public class Ex14_7 {
 				"How are you?"
 		};
 		
-		Stream<String> lineStream = Arrays.stream(lineArr);
-		lineStream.flatMap(l -> Stream.of(l.split(" +")))
-			.map(String::toLowerCase)
-			.distinct()
-			.sorted()
+		Stream<String> lineStream = Arrays.stream(lineArr); // 배열을 스트림으로 변환
+		lineStream.flatMap(l -> Stream.of(l.split(" +"))) // flatMap split으로 공백 기준으로 다 가져옴
+			.map(String::toLowerCase) // 소문자로
+			.distinct() // 중복제거
+			.sorted() // 순차정렬 abcd ... ~ 
 			.forEach(System.out::println);
 		
 		System.out.println();

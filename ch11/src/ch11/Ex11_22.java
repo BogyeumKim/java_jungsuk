01a42e263cb8d3831ee7c8baf7758525f88a16da
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-02-21 22:35:33 +0900

    Set 예제
    set은 중복허용하지 않으며 순서를 보장하지 않는다.

---
+++ ch11/src/ch11/Ex11_22.java
@@ -0,0 +1,26 @@
+package ch11;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Ex11_22 {
+    public static void main(String[] args) {
+        Set set = new HashSet();
+        int[][] board = new int[5][5];
+
+        for (int i = 0; set.size() < 25; i++) {
+            set.add((int) (Math.random() * 50) + 1 + "");
+        }
+        Iterator it = set.iterator();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = Integer.parseInt((String) it.next()); // next() 반환값은 Object타입이므로 형변환하여야함.
+                System.out.print((board[i][j] < 10 ? " " : " ") + board[i][j]);
+            } // for
+            System.out.println();
+        } // for
+    }
+}

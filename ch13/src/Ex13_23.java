5b315a0f4f6f34d9b8cc4dcb5638f949f66a5188
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-02-28 00:18:09 +0900

    ☆쓰레드 Lock, Condition 동기화 예제 -4
    기존 wait() , notify() 는 waitingfool에서 요리,손님 쓰레드를 구분할수없어 한쪽만 나오는 기아현생이 올수있는데
    lock와 Contion을 이용하면 각각의 쓰레드를 통제할수있다.

--- ch13/src/Ex13_23.java
+++ ch13/src/Ex13_23.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Ex13_23 {
     public static void main(String[] args) throws Exception {
@@ -69,31 +71,42 @@ class 테이블 {
     final int MAX_FOOD = 6; // 테이블에 놓을수있는 최대 음식 개수
 
     private ArrayList<String> 접시 = new ArrayList<>();
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition forCook = lock.newCondition();
+    private Condition forCust = lock.newCondition();
 
-    public synchronized void 추가(String 음식) { // 동기화 추가
-        while (접시.size() >= MAX_FOOD) { // 테이블접시에 음식 다찰때까지
-            String name = Thread.currentThread().getName();
-            System.out.println(name + " 은 기다리고있습니다. 추가파트 ");
-            try {
-                wait(); // 요리 쓰레드를 기다리게한다.
-                Thread.sleep(500);
-            } catch (InterruptedException e ){}
+    public  void 추가(String 음식) { // 동기화 추가
+        lock.lock(); // lock 시작 unlock으로 끝나는 부분까지 임계영역 이며 lock 이후는 try finally로 묶어야함.
+        try {
+            while (접시.size() >= MAX_FOOD) { // 테이블접시에 음식 다찰때까지
+                String name = Thread.currentThread().getName();
+                System.out.println(name + " 은 기다리고있습니다. 테이블에 음식이 가득차서 ");
+                try {
+                    forCook.await(); // 요리 쓰레드를 기다리게 함.
+//                wait(); // 요리 쓰레드를 기다리게한다.
+                    Thread.sleep(500);
+                } catch (InterruptedException e ){}
+            }
+
+            접시.add(음식);
+            forCust.signal(); // 기다리고있는 손님을 깨움
+//        notify(); // 기다리고있는 손님을 깨우기 위함
+            System.out.println("접시에는 : "+ 접시.toString());
+        } finally {
+            lock.unlock(); // lock 해제
         }
-
-        접시.add(음식);
-        notify(); // 기다리고있는 손님을 깨우기 위함
-        System.out.println("접시에는 : "+ 접시.toString());
     }
 
     public void 삭제(String 음식) {
-
-        synchronized (this) {
+        lock.lock();
+//        synchronized (this) {
             String name = Thread.currentThread().getName();
-
+        try{
             while (접시.size() == 0) {
-                System.out.println(name + " 은 기다리고있습니다. 삭제파트 ");
+                System.out.println(name + " 은 기다리고있습니다. 테이블에 아무것도 없어서 ");
                 try {
-                    wait(); // 손님쓰레드를 기다리게 한다.
+                    forCust.await(); // 손님 쓰레드를 기다리게한다.
+//                    wait(); // 손님쓰레드를 기다리게 한다.
                     Thread.sleep(500);
                 } catch (InterruptedException e) {}
 
@@ -103,18 +116,22 @@ class 테이블 {
                 for (int i = 0; i < 접시.size(); i++) {
                     if (음식.equals(접시.get(i))) {
                         접시.remove(i);
-                        notify(); // 잠자고 있는 요리를 깨우기 위함
+                        forCook.signal(); // 잠자고있는 요리를 깨움
+//                        notify(); // 잠자고 있는 요리를 깨우기 위함
                         return;
                     }
                 } // for
 
                 try{
-                    System.out.println(name + " 은 기다리고있습니다. 삭제파트 TWO");
-                    wait(); // 원하는 음식이 없는 손님쓰레드를 기다리게한다.
+                    System.out.println(name + " 은 기다리고있습니다. 원하는 음식이 없어서.");
+                    forCust.await(); // 원하는 은식이 없는 손님쓰레드를 기다리게함.
+//                    wait(); // 원하는 음식이 없는 손님쓰레드를 기다리게한다.
                     Thread.sleep(500);
                 } catch (InterruptedException e){}
             }//while
-        } // synchronized
+        } finally {
+            lock.unlock();
+        }
     }
 
     int 음식들() { return 메뉴.length; }

b31bf69481879b64cf96bcabf3523d31cd8da10c
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2023-02-28 11:49:50 +0900

    람다식 function패키지 예제-2
    래퍼클래스 대신 기본형을 사용하는 함수형 인터페이스 예제

--- ch14/src/Ex14_5_1.java
+++ ch14/src/Ex14_5_1.java
@@ -1,39 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Ex14_5_1 {
     public static void main(String[] args) {
-        Supplier<Integer> s = () -> (int)(Math.random() *100) +1; // Supplier 매개변수 없고 반환값 있음
-        Consumer<Integer> c = i -> System.out.println(i + ", "); // Consumer 매개변수 있고 반환값 없음
-        Predicate<Integer> p = i -> i % 2 == 0; // Predicate 조건식 표현하는데 사용. 매개변수는 하나 반환타입 bollean
-        Function<Integer,Integer> f = i->i/10*10; // i의 일의자리를 없앰, 일반적인 함수
+        IntSupplier s = () -> (int)(Math.random() *100) +1;
+        IntConsumer c = i -> System.out.println(i + ", ");
+        IntPredicate p = i -> i % 2 == 0;
+        IntUnaryOperator op = i->i/10*10;
 
-        List<Integer> list = new ArrayList<>();
+        int[] list = new int[10];
         makeRandomList(s, list);
-        System.out.println(list);
+        System.out.println(Arrays.toString(list));
 
         printEvenNum(p, c, list);
-        List<Integer> newList = doSomething(f, list);
-        System.out.println(newList);
+        int[] newList = doSomething(op, list);
+        System.out.println(Arrays.toString(newList));
     }
 
-    static <T> List<T> doSomething(Function<T, T> f, List<T> list) { // Function 일반적인 함수 apply 사용
-        List<T> newList = new ArrayList<T>(list.size());
+    static int[] doSomething(IntUnaryOperator s, int[] list) { // Function 일반적인 함수 apply 사용
+        int[] newList = new int[list.length];
 
-        for (T i : list) {
-            newList.add(f.apply(i));
+        for (int i = 0; i < newList.length; i++) {
+            newList[i] = s.applyAsInt(list[i]); // IntUnaryOperator은 apply가 아니라 applyAsInt()
         }
         return newList;
     }
 
-    static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) { // Predicate 조건식 표현하는데 사용. 매개변수는 하나 반환타입 bollean test사용
+    static void printEvenNum(IntPredicate p, IntConsumer c, int[] list) { // Predicate 조건식 표현하는데 사용. 매개변수는 하나 반환타입 bollean test사용
 //         Consumer 매개변수 있고 반환값 없음 accept 사용
         System.out.print("[");
-        for (T i : list) {
+        for (int i : list) {
             if (p.test(i)) {
                 c.accept(i);
             }
@@ -41,9 +39,9 @@ public class Ex14_5_1 {
         System.out.println("]");
     }
 
-    static <T> void makeRandomList(Supplier<T> s, List<T> list) { // Supplier 매개변수 없고 반환값 있음 get사용
+    static void makeRandomList(IntSupplier s, int[] list) { // Supplier 매개변수 없고 반환값 있음 get사용
         for (int i = 0; i < 10; i++) {
-            list.add(s.get());
+            list[i] = s.getAsInt(); // IntSupplier는 get이 아니라 getAsInt()으로
         }
     }
 }
